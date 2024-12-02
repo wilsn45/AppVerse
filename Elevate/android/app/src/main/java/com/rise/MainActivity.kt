0ccd898862f525148ec6ac240a3305b8d0c0a414
Wilson.Shakya <wilson.shakya@angelbroking.com>
2024-12-02 18:56:39 +0530

    Renamed Few more classes

--- Elevate/android/app/src/main/java/com/rise/MainActivity.kt
+++ Elevate/android/app/src/main/java/com/rise/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rise
+package com.Elevate
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- Elevate/android/app/src/main/java/com/rise/MainApplication.kt
+++ Elevate/android/app/src/main/java/com/rise/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rise
+package com.Elevate
 
 import android.app.Application
 import com.facebook.react.PackageList
