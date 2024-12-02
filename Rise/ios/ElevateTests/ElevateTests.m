d20d4988edf4fafd34b8215f65c1cb18cea2fe85
Wilson.Shakya <wilson.shakya@angelbroking.com>
2024-12-02 18:25:57 +0530

    Renamed Project file and tested with iphone

--- Rise/ios/ElevateTests/ElevateTests.m
+++ Rise/ios/ElevateTests/ElevateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RiseTests : XCTestCase
+@interface ElevateTests : XCTestCase
 
 @end
 
-@implementation RiseTests
+@implementation ElevateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
