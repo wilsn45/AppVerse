6502aa234b28ccbef6c554fdd39974aee0725d31
Wilson.Shakya <wilson.shakya@angelbroking.com>
2024-12-08 16:31:58 +0530

    Accessibility Changes on ContentScreen and Content Detail

--- Elevate/views/Content/ContentScreen.tsx
+++ Elevate/views/Content/ContentScreen.tsx
@@ -376,19 +376,22 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.cardContainer}>
-            <TouchableOpacity onPress={() => handleCardPress(item)} style={styles.cardContent}>
+            <TouchableOpacity onPress={() => handleCardPress(item)} style={styles.cardContent}
+               accessibilityLabel={`Content Card: ${item.title}`}>
               <Text style={styles.contentText}>{item.title}</Text>
               <Text style={styles.contentDescription}>{item.description}</Text>
             </TouchableOpacity>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.iconButton} onPress={() => handleSave(item.id, item.title)}>
+              <TouchableOpacity style={styles.iconButton} onPress={() => handleSave(item.id, item.title)}
+                accessibilityLabel={savedCards.get(item.id) ?`Unsave Card`: 'Save Card'}>
                 <Ionicons
                   name={savedCards.get(item.id) ? 'bookmark' : 'bookmark-outline'}
                   size={24}
                   color={savedCards.get(item.id) ? theme.colors.green : theme.colors.primary}
                 />
               </TouchableOpacity>
-              <TouchableOpacity style={styles.iconButtonLike} onPress={() => handleLike(item.id, item.title)}>
+              <TouchableOpacity style={styles.iconButtonLike} onPress={() => handleLike(item.id, item.title)}
+                accessibilityLabel={likedCards.get(item.id) ? `Unlike Card. Total like ${item.likeCount}`: `Like Card. Total like ${item.likeCount}`}>
                 <Ionicons
                   name={likedCards.get(item.id) ? 'heart' : 'heart-outline'}
                   size={24}
@@ -396,7 +399,8 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
                 />
                 <Text>{item.likeCount}</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.iconButton} onPress={() => handleAddTask(item.id)}>
+              <TouchableOpacity style={styles.iconButton} onPress={() => handleAddTask(item.id)}
+                 accessibilityLabel={'Add Task'}>
                 <MaterialIcons name="add-task" size={24} color={theme.colors.primary}/>
               </TouchableOpacity>
             </View>
@@ -408,7 +412,7 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
     {/* Task Modal */}
 {/* Task Modal */}
 <Modal visible={isModalVisible} animationType="slide" transparent>
-      <View style={styles.modalContainer}>
+      <View style={styles.modalContainer}  accessibilityLabel={'Add Task Screen'}>
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>Add Task</Text>
           
@@ -425,7 +429,8 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
           <View style={styles.optionButtonContainer}>
             <TouchableOpacity
               style={[styles.optionButton, selectedTaskType === 1 && styles.selectedButton]}
-              onPress={() => setSelectedTaskType(1)} // 0 for Routine
+              onPress={() => setSelectedTaskType(1)}
+              accessibilityLabel={'Routine Task'}   // 0 for Routine
             >
               <Text style={[styles.optionButtonText, selectedTaskType === 1 && styles.selectedOptionButtonText]}>
                 Routine
@@ -434,6 +439,7 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
             <TouchableOpacity
               style={[styles.optionButton, selectedTaskType === 2 && styles.selectedButton]}
               onPress={() => setSelectedTaskType(2)} // 1 for Goal
+              accessibilityLabel={'Goal Task'} 
             >
               <Text style={[styles.optionButtonText, selectedTaskType === 2 && styles.selectedOptionButtonText]}>
                 Goal
@@ -452,6 +458,7 @@ const sendCancelAddTaskPEvent = async (contentId: String) => {
                     selectedSubTaskType === value && styles.subSelectedButton,
                   ]}
                   onPress={() => setSelectedSubTaskType(value)}
+                  accessibilityLabel={`Task Frequency ${value === 1 ? 'Daily' : value === 2 ? 'Weekly' : 'Monthly'}`} 
                 >
                   <Text style={[
                     styles.subOptionButtonText,
