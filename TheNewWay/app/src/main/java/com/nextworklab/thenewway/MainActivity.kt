15f64a8ab2115a4c9d4924f7446d9c50d97ae68d
wilsn45 <skk.wilson@gmail.com>
2026-03-25 18:43:16 +0530

    Added dummy project

---
+++ TheNewWay/app/src/main/java/com/nextworklab/thenewway/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.nextworklab.thenewway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nextworklab.thenewway.ui.theme.TheNewWayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TheNewWayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "User",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Welcome to TheNewWay, $name!",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Button(onClick = { /* Handle click */ }) {
+            Text(text = "Get Started")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheNewWayTheme {
+        Greeting("Android")
+    }
+}
